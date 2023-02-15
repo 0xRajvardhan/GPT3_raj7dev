59f5db15174ae60baef4918774a53955992d95bd
Rajvardhan <singhrajvardhan2003@gmail.com>
2023-02-15 12:01:23 +0530

    what gpt3 component done

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Feature } from '../../components';
 import './whatGPT3.css';
 
 const WhatGPT3 = () => {
     return (
-        <div>WhatGPT3</div>
+        <div className='gpt3__whatgpt3 section__margin' id='whatgpt3'>
+            <div className='gpt3__whatgpt3-feature'>
+                <Feature />
+            </div>
+            <div className='gpt3__whatgpt3-heading'>
+                <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+                <p>Explore The Library</p>
+            </div>
+            <div className='gpt3__whatgpt3-container'>
+                <Feature />
+                <Feature />
+                <Feature />
+            </div>
+        </div>
     )
 }
 
